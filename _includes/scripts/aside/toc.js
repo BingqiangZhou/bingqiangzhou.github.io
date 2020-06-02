985846c3ad6cb44c8e83efae972cd8124f1cbf4f
BingqiangZhou <1299050656@qq.com>
2020-06-02 16:24:30 +0800

    make toc font bigger

--- _includes/scripts/aside/toc.js
+++ _includes/scripts/aside/toc.js
@@ -36,7 +36,7 @@
       $('.col-aside.d-print-none.js-col-aside').hide();
     }
     else{
-      $('.col-aside.d-print-none.js-col-aside').css('font-size', '100%')
+      $('.col-aside.d-print-none.js-col-aside').css('font-size', '16px')
     }
 
 
